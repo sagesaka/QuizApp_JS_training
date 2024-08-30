0d97d3a85f0f57465867d676aa5128b4e7a93bf0
sagesaka <sagesaka@riprice.co.jp>
2024-08-30 16:54:26 +0900

    クイズの解答処理

--- main.js
+++ main.js
@@ -21,3 +21,39 @@ const question = [
     "1"
   ]
 ];
+
+const quiz = document.getElementById('question')
+const ans1 = document.getElementById('ans1')
+const ans2 = document.getElementById('ans2')
+const ans3 = document.getElementById('ans3')
+
+let correct = 0;
+let quizCount = 0;
+
+function quizSet() {
+  quiz.textContent = question[quizCount][0];
+  ans1.textContent = question[quizCount][1];
+  ans2.textContent = question[quizCount][2];
+  ans3.textContent = question[quizCount][3];
+  /* quiz=HTMLのquestion, question配列のn番目に置き換える */
+}
+
+quizSet();
+
+function answerPush(ans) {
+  if (ans == question[quizCount][4]) {
+    alert("正解!!");
+    correct++;
+  } else {
+    alert("おととい来やがれバカヤロー!!");
+  }
+  quizCount++;
+  if (quizCount == question.length) {
+    quiz.textContent = `正解数は${correct}でした!`;
+    ans1.textContent = "";
+    ans2.textContent = "";
+    ans3.textContent = "";
+  } else {
+    quizSet();
+  }
+}
